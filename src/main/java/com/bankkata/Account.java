5f199b5a57ef5a642d169804b552d0b1c6058eb5
ryandawsonuk <ryandawson@cantab.net>
2017-02-05 12:37:32 +0000

    remove UnsupportedOperationExceptions from Account so its methods can be called and we see printLine not called - ready to move on to decide what to do with Account

--- src/main/java/com/bankkata/Account.java
+++ src/main/java/com/bankkata/Account.java
@@ -7,16 +7,13 @@ public class Account {
 
     public void deposit(int amount){
 
-        throw new UnsupportedOperationException();
     }
 
     public void withdraw(int amount){
 
-        throw new UnsupportedOperationException();
     }
 
     public void printStatement(){
 
-        throw new UnsupportedOperationException();
     }
 }
